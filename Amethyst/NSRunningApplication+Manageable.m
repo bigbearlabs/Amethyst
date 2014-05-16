940aa85a480139771bf208e882cc8e7b34aa2d5c
Andy Park <sohocoke@gmail.com>
2014-05-16 14:58:00 +0200

    WIP podify.

--- Amethyst/NSRunningApplication+Manageable.m
+++ Amethyst/NSRunningApplication+Manageable.m
@@ -19,3 +19,15 @@
 }
 
 @end
+
+
+// duped from Silica 0.1.4 to unblock citrine-extract.
+@implementation NSRunningApplication (Silica)
+
+- (BOOL)isAgent {
+  NSURL *bundleInfoPath = [[self.bundleURL URLByAppendingPathComponent:@"Contents"] URLByAppendingPathComponent:@"Info.plist"];
+  NSDictionary *applicationBundleInfoDictionary = [NSDictionary dictionaryWithContentsOfURL:bundleInfoPath];
+  return [applicationBundleInfoDictionary[@"LSUIElement"] boolValue];
+}
+
+@end
