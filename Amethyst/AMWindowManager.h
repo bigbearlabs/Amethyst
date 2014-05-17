3400ee7d720494d8d600c5d4fa83e09cd70645ef
Andy Park <sohocoke@gmail.com>
2014-05-17 16:56:56 +0200

    refactor AX event handlers to a delegate interface.

--- Amethyst/AMWindowManager.h
+++ Amethyst/AMWindowManager.h
@@ -12,6 +12,8 @@
 // Object for managing the windows across all screens and spaces.
 @interface AMWindowManager : NSObject
 
+- (id)initWithDelegate:(id)delegate;
+
 - (void)toggleFloatForFocusedWindow;
 
 - (NSArray *)windowsForScreen:(NSScreen *)screen;
